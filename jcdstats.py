034b2f7de86407818f9fdb34db02a56b1a570e5a
nipil <nipil@users.noreply.github.com>
2016-03-16 08:29:59 +0100

    attach and detach samples db

--- jcdstats.py
+++ jcdstats.py
@@ -11,9 +11,11 @@ class MinMax(object):
 
     StationsDayTable = "stations_day"
 
-    def __init__(self, db):
+    def __init__(self, db, sample_schema):
         self._db = db
+        self._sample_schema = sample_schema
         assert(self._db is not None)
+        assert(self._sample_schema is not None)
         self._create_tables_if_necessary()
 
     def _create_tables_if_necessary(self):
@@ -53,24 +55,22 @@ class MinMax(object):
                 MIN(available_bike_stands),
                 MAX(available_bike_stands),
                 COUNT(timestamp)
-                FROM samples.archived_samples
+                FROM %s.%s
                 GROUP BY contract_id, station_number
-                ''' % self.StationsDayTable, (date,))
+                ''' % (self.StationsDayTable, self._sample_schema, jcd.dao.ShortSamplesDAO.TableNameArchive),
+                (date,))
         except sqlite3.Error as error:
             print "%s: %s" % (type(error).__name__, error)
             raise jcd.common.JcdException(
-                "Database error while creating table [%s]" % self.StationsDayTable)
+                "Database error while storing daily min max into table [%s]" % self.StationsDayTable)
 
     def run(self, date):
-        print "date", date
-        # TODO: attach samples db
         self._do_stations(date)
-        # TODO: detach samples db
 
 class App(object):
 
     def __init__(self, default_data_path, default_db_filename):
-        self.minmax = None
+        # construct parser
         self._parser = argparse.ArgumentParser(
             description='Calculate min-max data from jcd stats')
         self._parser.add_argument(
@@ -98,15 +98,24 @@ class App(object):
     def run(self):
         # parse arguments
         arguments = self._parser.parse_args()
-        with jcd.common.SqliteDB(arguments.dbname, arguments.datadir) as db:
-            self.minmax = MinMax(db)
+        print arguments
+        with jcd.common.SqliteDB(arguments.dbname, arguments.datadir) as db_stats:
             for date in arguments.date:
-                self.minmax.run(date)
+                if arguments.verbose:
+                    print "Processing", date
+                # attach samples db
+                schema = jcd.dao.ShortSamplesDAO.get_schema_name(date)
+                filename = jcd.dao.ShortSamplesDAO.get_db_file_name(schema)
+                print schema, filename
+                db_stats.attach_database(filename, schema, arguments.datadir)
+                # do processing
+                MinMax(db_stats, schema).run(date)
+                # detach samples db
+                db_stats.detach_database(schema)
 
 # main
 if __name__ == '__main__':
     try:
-        application = App("~/.jcd_v2", "stats.db")
-        application.run()
+        App("~/.jcd_v2", "stats.db").run()
     except KeyboardInterrupt:
         pass
